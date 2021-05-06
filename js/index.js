cbe7864f7be380a8796d751a0c9a997f616d649b
roieLopez <64579967+roieLopez@users.noreply.github.com>
2021-05-06 20:27:43 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,51 +1,46 @@
-"use strict";
-
 let input = document.querySelector('input');
 let btn17 = document.querySelector('.btnOne');
 let btn25 = document.querySelector('.btnTwo');
-let btn30 = document.querySelector('.btnThree');
 let message = document.querySelector('.message');
 
 
-Number.prototype.format = function (d, w, s, c) {
-    var re = '\\d(?=(\\d{' + (w || 3) + '})+' + (d > 0 ? '\\b' : '$') + ')',
-        num = this.toFixed(Math.max(0, ~~d));
+let calc17 = () => input.value * 0.17;
+let calc25 = () => input.value * 0.25;
+let amountCalc = () => calc17() + Number(input.value);
 
-    return (c ? num.replace('.', c) : num).replace(new RegExp(re, 'g'), '$&' + (s || ','));
-};
 
 btn17.addEventListener('click', function () {
-    let x = input.value;
-    if (x) {
-        let amountCalc = x * 0.17;
-        let amount = input.value;
-        var r = amountCalc + Number(amount);
+    if (input.value = input.value) {
         message.innerHTML = `
-        <div>  סכום לפני מע"מ : ${Number(amount).format()} &#8362 </div>
-        <div class="result"> סכום מע"מ : ${amountCalc.format()} 
-        <span class="resultICon">&#8362</span>
-         </div> 
-         <div>סכום אחרי מע"מ : ${r.format()} &#8362 </div>`;
-         console.log(amount);
-    } else {
-        console.log('err')
-    }
-})
 
+        <div>  סכום לפני מע"מ :
+         ${new Intl.NumberFormat({style: 'decimal'}).format(input.value)} &#8362 
+         </div>
+
+        <div class="result"> 
+        סכום מע"מ :  ${new Intl.NumberFormat({style: 'decimal'}).format(calc17())} &#8362
+         </div> 
+
+         <div>
+         סכום אחרי מע"מ : ${new Intl.NumberFormat({style: 'decimal'}).format(amountCalc())} &#8362 
+         </div>`
+    } else {
+        console.log('error')
+    }
+});
 
 btn25.addEventListener('click', function () {
-    let x = input.value;
-    if (x) {
-        let amountCalc = x * 0.25;
-        let amount = input.value;
-        var r = amountCalc + Number(amount);
+    if (input.value = input.value) {
         message.innerHTML = `
-        <div>  ערך הנכס : ${Number(amount).format()} &#8362 </div>
-        <div class="result"> הון עצמי נדרש : ${amountCalc.format()} 
-        <span class="resultICon">&#8362</span>
-         </div> `;
-        console.log(amount);
+    
+        <div> 
+         ערך הנכס : ${new Intl.NumberFormat({style: 'decimal'}).format(input.value)} &#8362
+         </div>
+
+        <div class="result"> 
+        הון עצמי נדרש : ${new Intl.NumberFormat({style: 'decimal'}).format(calc25())} &#8362
+         </div> `
     } else {
-        console.log('err')
+        console.log('error')
     }
-})
+});
